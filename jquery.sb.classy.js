c275633db5357ba1bf6836091222bd2cf729e4a0
Max Schnur <max.schnur@gmail.com>
2011-01-23 18:22:19 -0500

    started implementing a js class-based SelectBox plugin. some parts of it are nice--it helps clarify a lot of the markup generation--but i feel like i'm rewriting pieces of code that jQuery already handles nicely. i might just need to use one class (so i can link it with $.proto) and improve the existing codebase in that.

---
+++ jquery.sb.classy.js
@@ -0,0 +1,19 @@
+(function($) {
+  
+  var SelectBox = Class.extend({
+    
+  });
+  
+  function Display() {
+  }
+  
+  function Dropdown() {
+  }
+  
+  function Option() {
+  }
+  
+  $.fn.sb = function(options) {
+    
+  };
+}(jQuery));
